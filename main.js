671f7962eb719f4e587ed2e078b44a01c5dae4a3
Sebastian Miozga <miozga.sebastian@gmail.com>
2025-02-21 17:16:26 +0100

    abc

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const spnText = document.querySelector(".text");
+const spnCursor = document.querySelector(".cursor");
 const txt =
-  "fghdthgikjhfghfg tydjdfgjdtgjsfdytjsftgh jtdhdfgujrty rtjhtghdsfgu tyhdgyf kldjfvoidfjg hjkgijjhngkmdfgvnbiudsfg dfjkghd9ugh bvkjdhgosdfibv dsijghsdituf";
+  "fghdthgikjhfghfg tydjdfgjdtgjsfdytjsftgh jtdhdfgujrty rtjhtghdsfgu tyhdgyf kldjfvoidfjg hjkgijjhngkmdfgvnbiudsfg dfjkghd9ugh bvkjdhgosdfibv dsijghsdituf kljvdsiofjgpore jiguyher voijfdgmfd dfgmodfkvbjiersug dfokgjfdoigjsflkgmhnbofsigkbn fdkghjfodihjodfih oklfjgbokfgmbokfgmh ofglkmb folkghbmfokgbmf bofkghjfokmbf b opfgjmb fpgnm  KONIEC";
 
 let indexText = 0;
 const time = 120;
@@ -10,4 +11,9 @@ const addLetter = () => {
 
   if (indexText === txt.length) clearInterval(indexTyping);
 };
+const cursorAnimation = () => {
+  spnCursor.classList.toggle("active");
+};
+
 const indexTyping = setInterval(addLetter, time);
+setInterval(cursorAnimation, 400);
